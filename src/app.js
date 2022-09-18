495819867aef19d04759fce8b4a4966a7e4f51d1
khrystynadatsko <105358551+khrystynadatsko@users.noreply.github.com>
2022-09-18 16:57:23 +0300

    added js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+}
 
+let apiKey = "ea47164ae5be84043ec6486a70a6b264";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);

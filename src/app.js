9ec8424de0c8e8b032c6d9c0a22ebe6ee1212d6e
khrystynadatsko <105358551+khrystynadatsko@users.noreply.github.com>
2022-09-18 23:06:14 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -51,8 +51,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "ea47164ae5be84043ec6486a70a6b264";
-let city = "Lviv";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "ea47164ae5be84043ec6486a70a6b264";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Lviv");
